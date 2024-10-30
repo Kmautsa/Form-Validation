08d060b01840bf74fbfb3a9a24a8c539b9ac8647
mautsa,Kudzanai <149285204+Kmautsa@users.noreply.github.com>
2024-10-29 21:32:41 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+const form = document.getElementById('myForm');
+const elements = form.elements;
+
+function validateForm() {
+    for (let i = 0; i < elements.length; i++) {
+        if (elements[i].value === "") {
+            console.log("This input is empty!");
+        } else {
+            console.log("This input has some data: " + elements[i].value);
+        }
+    }
+}
+
+form.addEventListener('submit', function(event) {
+    event.preventDefault(); // Prevents form submission
+    validateForm();
+});
